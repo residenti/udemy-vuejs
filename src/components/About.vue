528abc45a39fcf7a3e81dc36b58f8073f6722b96
residenti <n.queue.r@gmail.com>
2019-12-14 20:24:07 +0900

    [動的コンポーネント] ライフサイクルメソッドactivatedとdeactivated.

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,8 +4,36 @@
 
 <script>
 export default {
+  beforeCreate() {
+    console.log("beforeCreate!!")
+  },
+  created() {
+    console.log("created!!")
+  },
+  beforeMount() {
+    console.log("beforeMount!!")
+  },
+  mounted() {
+    console.log("mounted!!")
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate!!")
+  },
+  updated() {
+    console.log("updated!!")
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy!!")
+  },
   destroyed() {
     console.log("destroyed!!")
+  },
+
+  activated() {
+    console.log("activated!!")
+  },
+  deactivated() {
+    console.log("deactivated!!")
   }
 }
 </script>
