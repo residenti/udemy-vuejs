e4d14f7fc77439153806d2f3bb72fcb9cf937475
residenti <n.queue.r@gmail.com>
2020-01-06 15:49:23 +0900

    ボタンで router view を切り替える.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,16 @@
 <template>
-  <p>Home</p>
+  <div>
+    <p>Home</p>
+    <button @click="toAbout">Aboutページにいく.</button>
+  </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      toAbout() {
+        this.$router.push('about')
+      }
+    }
+  }
+</script>
