5fc7a057c509c1f52f1494a15ef220bceb247c98
residenti <n.queue.r@gmail.com>
2020-01-06 11:43:54 +0900

    vue router のモードを hash (デフォルト) から history に変更.

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import About from './components/About.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     { path: '/', component: Home },
     { path: '/about', component: About }
