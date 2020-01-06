39e45a4555e862c48bb72f078d55882702802558
residenti <n.queue.r@gmail.com>
2020-01-06 22:32:46 +0900

    202. 全てのページ遷移前に、特定の処理をするためのbeforeEachガードの説明.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 全てのページ遷移前に特定の処理をする.
+router.beforeEach((to, from, next) => {
+  console.log("global-beforeEach")
+  next()
+})
+
 new Vue({
   router: router,
   render: h => h(App),
