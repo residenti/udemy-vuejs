d3f7419d8a55e01de3ff362752a8cf9d9e2baf89
residenti <n.queue.r@gmail.com>
2020-01-07 15:50:33 +0900

    212. mutationとcommitを使って、状態(state)を変える場所を一つに絞りデータの予測と追跡をしやすくする.

--- src/store.js
+++ src/store.js
@@ -4,11 +4,24 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
   state: {
     count: 2
   },
+
+  // データ(state)の変更を行う処理は必ず mutations に定義する.
+  mutations: {
+    increment(state, number) {
+      state.count += number
+    },
+    decrement(state, number) {
+      state.count -= number
+    }
+  },
+
   getters: {
     doubleCount: state => state.count * 2,
     tripleCount: state => state.count * 3
   }
+
 })
